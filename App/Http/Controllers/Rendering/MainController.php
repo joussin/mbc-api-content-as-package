a758360ca22f4b05c0dcc101a6df8b3fb9ba310f
stephane_joussin <stephane@cartegriseminute.fr>
2023-01-26 04:51:42 +0100

    export

--- App/Http/Controllers/Rendering/MainController.php
+++ App/Http/Controllers/Rendering/MainController.php
@@ -13,48 +13,31 @@ class MainController extends Controller
 {
 
 
-
-    public function list(Request $request)
-    {
-
-        $routeEntityCollection = RouterFacade::getRouteEntityCollection();
-
-        $routeEntityCollection->each(function($routeEntity){
-
-            echo '<a href="'.$routeEntity->getUri().'" >' . $routeEntity->getName() . '</a><br>';
-        });
-
-    }
-
-    public function dynamic(Request $request, $id)
-    {
-
-
-        dd('dynamic', $request->route());
-    }
-
     public function any(Request $request)
     {
-
-        $ar = get_object_vars($request);
-
-        Log::info("log request as array", $ar);
-        Log::info("log request as array", [
-            $request->route()->getController(),
-            $request->route()->getActionMethod(),
-            $request->route()->getActionName(),
-            $request->route()->getActionName(),
-        ]);
-
-        $json = file_get_contents("https://dummyjson.com/posts");
-
-        $data = json_decode($json, true);
-
-
-
-
-        return view('welcome2', [
-            'data' => $data
-        ]);
+        return 'MainController-any';
     }
+
+
+//    public function test(Request $request)
+//    {
+//
+//        $ar = get_object_vars($request);
+//
+//        Log::info("log request as array", $ar);
+//        Log::info("log request as array", [
+//            $request->route()->getController(),
+//            $request->route()->getActionMethod(),
+//            $request->route()->getActionName(),
+//            $request->route()->getActionName(),
+//        ]);
+//
+//        $json = file_get_contents("https://dummyjson.com/posts");
+//
+//        $data = json_decode($json, true);
+//
+//        return view('welcome2', [
+//            'data' => $data
+//        ]);
+//    }
 }
