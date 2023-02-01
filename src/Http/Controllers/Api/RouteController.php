441b6f0dc0c5d44958a696ee9b79bd8c1192f38b
stephane_joussin <stephane@cartegriseminute.fr>
2023-02-01 11:19:05 +0100

    rest resource laravel

---
+++ src/Http/Controllers/Api/RouteController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace MbcApiContent\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use MbcApiContent\Http\Resources\RouteResource;
+use MbcApiContent\Models\Route;
+
+class RouteController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return RouteResource::collection(Route::all());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $route = Route::create([
+            'user_id' => $request->user()->id,
+            'title' => $request->title,
+            'description' => $request->description,
+        ]);
+
+        return new RouteResource($route);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+//    public function show($id)
+    public function show(Route $route)
+    {
+        return new RouteResource($route);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $route)
+    {
+        $route->update($request->only(['title', 'description']));
+
+        return new RouteResource($route);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($route)
+    {
+        $route->delete();
+
+        return response()->json(null, 204);
+    }
+}
